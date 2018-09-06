e021cac6189294ff71608a478ae4e7ba98277654
Evan <evan@debuggered.com>
2018-09-06 16:33:08 -0700

    updates for d10

--- 2017/day10/day10a.js
+++ 2017/day10/day10a.js
@@ -4,69 +4,55 @@ const aoc = require('../lib/aoc.js');
 const instruct = aoc.inputfile('./day10.txt');
 
 let listsize = 256, skipsize = 0, circle = [], current = 0;
-let instructions = instruct.split(' ');
+let instructions = instruct.split(',');
 
+// Create array
 for (i=0; i<listsize; i++){
 	circle.push(i);
 }
-console.log(circle);
-console.log(circle[0] * circle[1]);
 
-instructions.forEach( steps => {
-	let steps_to_end = listsize - current;
-	let overflow = steps - steps_to_end;
+instructions.forEach(step => {
+	step = parseInt(step);
 
-	let newcurrent = current + (steps - 1);
-
-	if (overflow > 0) {
-		let frontstep = overflow - 1;
+	circle = knothash(circle,current,step);
+	console.log(current, step, skipsize, (current + step - circle.length))
+	if (current + step <= circle.length) {
+		current += step;
+	} else {
+		current = (current + step - circle.length);
 	}
-
+	current += skipsize;
+	skipsize += 1;
 })
 
-/*
-set fsize [file size "day10.txt"]
-set fp [open "day10.txt" r]
-set instructions [read $fp $fsize]
-close $fp
+console.log('result: ',circle[0] * circle[1])
 
-set listsize 256
-set circle {0}
-set current 0
-set offset 0
+// Create knot
+function knothash(array, current, step){
+	let asize = array.length;
+	let sliceend = current + step;
 
-for {set x 1} {$x < $listsize} {incr x} { lappend circle $x }
-set instructions [expr {[split $instructions ',']}]
+	if(current + step <= asize) {
+		let lastend = asize - 1;
 
-foreach steps $instructions {
-	set steps_to_end [expr {$listsize - $current}]
-	set remainder [expr {$steps - $steps_to_end}]
-	set frontstop [expr {$remainder - 1}]
-	set endstop [expr {$current + ($steps - 1)}]
+		let first 	= (current != 0)? 			array.slice(0,current)			: [];
+		let middle 	= (step > 0)? 				array.slice(current, sliceend)	: [];
+		let last 	= (sliceend <= asize)?	array.slice(sliceend,lastend)	: [];
 
-	set endlist [expr {[lrange $circle $current $endstop]}]
-
-	if {$remainder > 0} {
-		set frontlist [expr {[lrange $circle 0 $frontstop]}]
-		set endlist [expr {[concat $endlist $frontlist]}]
-	}
-
-	foreach j [expr {[lreverse $endlist]}] {
-		lset circle $current $j
-		if {($current + 1) >= $listsize} {
-			set current 0
-		} else {
-			set current [expr {$current + 1}]
-		}
-	}
-	if {$current + $offset >= $listsize} {
-		set current [expr {($offset + $current) - $listsize}]
+		return first.concat(middle.reverse(),last);
 	} else {
-		set current [expr {$current + $offset}]
+		let remainder = (current + step) - asize;
+
+		let first = array.slice(0,remainder);
+		let middle = array.slice(remainder, current);
+		let last = array.slice(current,array.length - 1);
+		let wholeslice = first.concat(last);
+			wholeslice = wholeslice.reverse();
+
+			first = wholeslice.slice(0,remainder);
+			last = wholeslice.slice(remainder, step);
+
+		return first.concat(middle,last);
+
 	}
-	incr offset
-
 }
-puts [expr {[lindex $circle 0] * [lindex $circle 1]}]
-
-*/
