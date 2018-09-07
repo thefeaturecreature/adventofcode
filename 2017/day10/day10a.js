890473efaeef4368f73a2cbbf2faf82b85319f74
Evan Dierlam <evan@debuggered.com>
2018-09-07 09:51:19 -0700

    day10

--- 2017/day10/day10a.js
+++ 2017/day10/day10a.js
@@ -1,58 +1,15 @@
 // TODO: Finish
-
 const aoc = require('../lib/aoc.js');
 const instruct = aoc.inputfile('./day10.txt');
-
-let listsize = 256, skipsize = 0, circle = [], current = 0;
 let instructions = instruct.split(',');
 
+let circle = [];
+
 // Create array
-for (i=0; i<listsize; i++){
+for (i=0; i<256; i++){
 	circle.push(i);
 }
 
-instructions.forEach(step => {
-	step = parseInt(step);
-
-	circle = knothash(circle,current,step);
-	console.log(current, step, skipsize, (current + step - circle.length))
-	if (current + step <= circle.length) {
-		current += step;
-	} else {
-		current = (current + step - circle.length);
-	}
-	current += skipsize;
-	skipsize += 1;
-})
+circle = aoc.stepknots(circle,instructions);
 
 console.log('result: ',circle[0] * circle[1])
-
-// Create knot
-function knothash(array, current, step){
-	let asize = array.length;
-	let sliceend = current + step;
-
-	if(current + step <= asize) {
-		let lastend = asize - 1;
-
-		let first 	= (current != 0)? 			array.slice(0,current)			: [];
-		let middle 	= (step > 0)? 				array.slice(current, sliceend)	: [];
-		let last 	= (sliceend <= asize)?	array.slice(sliceend,lastend)	: [];
-
-		return first.concat(middle.reverse(),last);
-	} else {
-		let remainder = (current + step) - asize;
-
-		let first = array.slice(0,remainder);
-		let middle = array.slice(remainder, current);
-		let last = array.slice(current,array.length - 1);
-		let wholeslice = first.concat(last);
-			wholeslice = wholeslice.reverse();
-
-			first = wholeslice.slice(0,remainder);
-			last = wholeslice.slice(remainder, step);
-
-		return first.concat(middle,last);
-
-	}
-}
--- 2017/lib/aoc.js
+++ 2017/lib/aoc.js
@@ -12,4 +12,48 @@ module.exports = {
         callback(wordA, wordB, indexA, indexB))
     })
   },
+
+  knothash: (array, current, step) => {
+    let asize = array.length;
+    let sliceend = current + step;
+  
+    if(sliceend <= asize) {
+      let lastend = asize;
+  
+      let first 	= (current != 0)? array.slice(0,current) : [];
+      let middle 	= (step > 0)? array.slice(current, sliceend) : [];
+      let last 	= (sliceend <= asize)? array.slice(sliceend,lastend) : [];
+  
+      return first.concat(middle.reverse(),last);
+    } else {
+      let remainder = sliceend - asize;
+  
+      let first = array.slice(0,remainder);
+      let middle = array.slice(remainder, current);
+      let last = array.slice(current,asize);
+      let wholeslice = last.concat(first);
+        wholeslice = wholeslice.reverse();
+  
+        last = wholeslice.slice(0,step - remainder);
+        first = wholeslice.slice(wholeslice.length - remainder, asize);
+        
+      return first.concat(middle,last);
+    }
+  },
+
+  stepknots: (array,steps) => {
+    let skipsize = 0, current = 0;
+    steps.forEach(step => {
+      step = parseInt(step);
+      array = knothash(array,current,step);
+      current += step;
+      current += skipsize;
+      skipsize += 1;
+    
+      if (current >= array.length) {
+        current = current - array.length;
+      }
+    })
+    return array;
+  },
 }
