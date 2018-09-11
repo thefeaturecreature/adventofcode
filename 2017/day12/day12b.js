dd855102d8c99a6b411a2a1f228f293fd37a604f
Evan <evan@debuggered.com>
2018-09-11 11:13:53 -0700

    day12

--- 2017/day12/day12b.js
+++ 2017/day12/day12b.js
@@ -1,38 +1,28 @@
-/*
-set fsize [file size "day12.txt"]
-set fp [open "day12.txt" r]
-set pipes [read $fp $fsize]
-close $fp
+const aoc = require('../lib/aoc.js');
+let pipes = inputfile('./day12.txt').split('\n');
+let well = [0];
+let count = 0;
 
-# Search for first not in queue -- if none break
-# loop using that as beginning
-# incr queuecount
-
-
-set pipes [split $pipes '\n']
-set queuecount 0
-# [llength $pipes]
-for {set x 0} {$x < [llength $pipes]} {incr x} {
-  set line [lindex $pipes $x]
-  if {$line != {d}} {
-    incr queuecount
-    set queue [lindex [split $line " ,<->"] 0]
-    for {set y 0} {$y < [llength $queue]} {incr y} {
-      set target [lindex $pipes [lindex $queue $y]]
-      set branch [split $target ",>"]
-      set j 0
-      foreach i $branch {
-        if {$j >= 1} {
-          set newnum [string trim $i]
-          if {[lsearch $queue $newnum] == -1} {
-            lappend queue $newnum
-          }
-        }
-        incr j
-      }
-      lset pipes [lindex $queue $y] d
+for(i=0;i<pipes.length;i++){
+  next = pipes[i].split(' <-> ')[0]
+  if(!well.includes(next)) {
+    well.push(next)
+    count++;
+    for(j = well.length - 1;j<well.length;j++){
+      item = well[j];
+      pipes
+        .filter(s => s.startsWith(`${item} <->`))
+        .forEach(swap => {
+        swap
+          .split(' <-> ')[1]
+          .split(', ')
+          .forEach(item => {
+            if(!well.includes(item) && item > 0) {
+              well.push(item)
+            }
+          })
+      })
     }
   }
 }
-puts $queuecount
-*/
+console.log(count)
