e7f0336d13e4e220e61736ffcc407a48aa293c1a
Evan <evan@debuggered.com>
2018-09-12 12:07:19 -0700

    day 13 complete

--- 2017/day13/day13a.js
+++ 2017/day13/day13a.js
@@ -1,53 +1,24 @@
 const aoc = require('../lib/aoc.js');
 const levels = inputfile('./day13.txt');
-let totals = 0;
 
-levels.split('\n');
-
-let heartbeat = (size,ticks) => {
-  let col = Math.trunc((ticks - 1) / (size - 1))
-  let rem = ticks % size ;
-  let maxind = size - 1;
-
-  console.log('---')  
-  console.log(`Tick: ${ticks}`)
-  console.log(`Col: ${col}`)
-  console.log(`Rem: ${rem}`)
-
-  if(col == 0) return rem;
-  if(col % 2 == 0) {
-   // return rem;
-  } else {
-    return (maxind - 1 - rem)
-  }
-}
-let s2 = 4
-let ender = 15
-for(i=0;i<ender;i++){
-  console.log(`Heartbeat: ${heartbeat(s2,i)}`)
+let whereami = (size,ticks) => {
+  let maxint = size - 1;
+  let col = Math.trunc(ticks / maxint)
+  let rem = ticks - (maxint * col) ;
+  return (col % 2 == 0)? rem : (maxint - rem);
 }
 
-/*
-set total 0
 
-for {set x 0} {$x < [llength $levels]} {incr x} {
-  set level [lindex $levels $x]
-  set step [lindex [split $level ":"] 0]
-  set height [string trim [lindex [split $level ":"] 1]]
-  set full [expr {(2 * $height) - 2}]
-  if {$step > $full} {
-    set remstep [expr {$step % $full}]
-  } else {
-    set remstep $step
-  }
-  if {$remstep >= $height} {
-      set round [expr {($height - 1) - ($remstep - ($height - 1))}]
-    } else {
-      set round $remstep
-    }
-  if {$round == 0} {
-    incr total [expr {$height * $step}]
-  }
+let severity = start => {
+  return levels
+  .split('\n')
+  .reduce((total, step) => {
+    parts = step
+      .split(': ')
+      .map(part => parseInt(part));
+      return ((whereami(parts[1],parts[0] + start) == 0)?
+      (parts[0] * parts[1]) : 0) + total;
+  }, 0)
 }
-puts $total
-*/
+
+console.log(severity(0))
