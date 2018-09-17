bb89ac3b90ebcc4d9fa0b6c5e61b0a466da1d6a6
Evan Dierlam <evan@debuggered.com>
2018-09-16 23:13:42 -0700

    Day 17

--- 2017/day17/day17a.js
+++ 2017/day17/day17a.js
@@ -1,10 +1,12 @@
+const aoc = require('../lib/aoc.js');
+
 let myval = 366
   max = 2017;
-  list = [0];
   current = 0
+  list = [0];
+  
 for (i = 1; i <= max ; i++) {
-  current = (current + myval + 1) % i;
+  current = roundthesun(i, current, myval + 1);
   list.splice(current, 0, i);
 }
-
 console.log(list[list.indexOf(max) + 1]);
--- 2017/day17/day17b.js
+++ 2017/day17/day17b.js
@@ -1,10 +1,12 @@
+const aoc = require('../lib/aoc.js');
+
 let myval = 366
   max = 50000000;
   current = 0;
   uno = 1;
 
 for (i = 1; i <= max ; i++) {
-  current = (current + myval + 1) % i;
+  current = roundthesun(i, current, myval + 1);
   if (current == 0) uno = i;
 }
 console.log(uno);
