27e4ccd69e0d0f0d8d74175cfde6f665f1fac9b7
Evan Dierlam <evan@debuggered.com>
2019-11-19 23:29:29 -0800

    day 5 in js

--- 2018/day05/day05a.js
+++ 2018/day05/day05a.js
@@ -1,20 +1,17 @@
 const aoc = require('../lib/aoc.js');
-let polymer =  'dabAcCaCBAcCcaDA'; //inputfile('./day05.txt');
+let polymer =  inputfile('./day05.txt');
+let regs = []
 
-for(h = 10; h <= 34; h++){
-  reg1 = new RegExp((h).toString(36) + (h).toString(36).toUpperCase(), 'g');
-  reg2 = new RegExp((h).toString(36).toUpperCase() + (h).toString(36), 'g');
-  while(!reg1.test(polymer) && !reg2.test(polymer)) {
-    for(i = 10; i <= 34; i++){
-      reg3 = new RegExp((i).toString(36) + (i).toString(36).toUpperCase(), 'g');
-      reg4 = new RegExp((i).toString(36).toUpperCase() + (i).toString(36), 'g');
-      while(!reg3.test(polymer) && !reg4.test(polymer)) {
-        polymer = polymer
-          .replace(reg3,'')
-          .replace(reg4,'')
-      }
-    }
-    }
-console.log(h)
+for (h = 10; h <= 35; h++){
+  regletter = (h).toString(36);
+  regs.push(regletter + regletter.toUpperCase())
+  regs.push(regletter.toUpperCase() + regletter)
 }
-console.log(polymer)
+
+while (true) {
+  prevcycle = polymer
+  regs.forEach(reg => polymer = polymer.replace(new RegExp(reg, 'g'),''))
+  if(prevcycle == polymer) break;
+}
+console.log(polymer.length);
+
