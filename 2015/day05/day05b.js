f29afd0d8a62ebd0f4cc828650d5103484e65ad2
Evan Dierlam <edierlam@edierlam-nw3354.corp.nerdwallet.com>
2019-11-21 09:28:32 -0800

    d5.b

--- 2015/day05/day05b.js
+++ 2015/day05/day05b.js
@@ -5,16 +5,21 @@ let nicelist = new Set,
     nicesplit = false;
 
 sortlist.forEach(word => {
-    [...word].forEach( (letter, index) => {
+    nicesplit = false;
+    nicepair = false;
+    [...word].forEach( (letter, index, splitword) => {
         if(letter == word.charAt(index + 2)) nicesplit = true;
+        
+        letterpair = letter + word.charAt(index + 1)
+        splitword.forEach( (loopletter, loopindex) => {
+            if (index != loopindex && index != loopindex - 1 && index != loopindex + 1) {
+                looppair = loopletter + word.charAt(loopindex + 1)
+                if(looppair == letterpair) nicepair = true
+            }
+        })
     })
-    
 
 
+    if (nicesplit && nicepair) nicelist.add(word)
 })
 console.log(nicelist.size)
-
-/*
-It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
-It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
-*/
