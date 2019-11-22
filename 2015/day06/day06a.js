3273b12fa8974ba57d73c95cbc7123b2c2cf3768
Evan Dierlam <edierlam@edierlam-nw3354.corp.nerdwallet.com>
2019-11-22 14:08:10 -0800

    2015/d06 incomplete

--- 2015/day06/day06a.js
+++ 2015/day06/day06a.js
@@ -1,34 +1,19 @@
 const aoc = require('../lib/aoc.js');
-// const lightpath = ['turn on 0,0 through 999,999','toggle 0,0 through 999,0','turn off 499,499 through 500,500']
+//const lightpath = ['turn on 0,0 through 999,999','toggle 0,0 through 999,0','turn off 499,499 through 500,500']
 const lightpath = inputfile('./day06.txt', true);
 let lightgrid = new Set();
 
 lightpath.forEach(instr => {
-    numbers = instr.match(/(toggle|turn on|turn off) ([\d]+),([\d]+) through ([\d]+),([\d]+)/)
- 
-    switch (numbers[1]) {
-        case "toggle":
-            switch_on = true;
-            switch_off = true;
-            break;
-        case "turn on":
-            switch_on = true;
-            switch_off = false;
-            break;
-        case "turn off":
-            switch_on = false;
-            switch_off = true;
-            break;
-    }
+    inst_parts = instr.match(/(toggle|turn on|turn off) ([\d]+),([\d]+) through ([\d]+),([\d]+)/)
 
-    for(x = numbers[2];x <= numbers[4]; x++) {
-        for(y = numbers[3];y <= numbers[5]; y++) {
+    for(x = inst_parts[2];x <= inst_parts[4]; x++) {
+        for(y = inst_parts[3];y <= inst_parts[5]; y++) {
             plot = x.toString() + "x" + y.toString();
 
             if(lightgrid.has(plot)){ 
-                if (switch_off) lightgrid.delete(plot)
+                if (inst_parts[1] !== "turn on") lightgrid.delete(plot)
             } else {
-                if (switch_on) lightgrid.add(plot)
+                if (inst_parts[1] !== "turn off") lightgrid.add(plot)
             }
 
         }
