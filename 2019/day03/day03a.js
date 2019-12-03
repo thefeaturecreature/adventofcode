5cacddb7037a0f42fcc13dea5c219af141b143dd
Evan Dierlam <edierlam@edierlam-nw3354.corp.nerdwallet.com>
2019-12-03 12:29:37 -0800

    refac day3

--- 2019/day03/day03a.js
+++ 2019/day03/day03a.js
@@ -1,79 +1,57 @@
+var _ =  require('lodash');
 const aoc = require('../lib/aoc.js');
-const wires = inputfile('./day03.txt'); // not 517
-const wirest1 = ['R8,U5,L5,D3','U7,R6,D4,L4'] // = 6
-const wirest2 = ['R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D58,R83'] // = 159
-const wirest3 = ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51','U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'] // = 135
+const wires = inputfile('./day03.txt');
+const wirest1 = ['R8,U5,L5,D3','U7,R6,D4,L4'];
+const wirest2 = ['R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D58,R83'];
+const wirest3 = ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51','U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'];
 
-let wire1 = new Set(),
-    currentx, currenty, closest;
+let wirepath = new Set(),
+    x, y, closest;
 
-let addwires = (currentx,currenty) => {
-    wire1.add(currentx + 'x' + currenty)
+let addwires = (x,y) => {
+    wirepath.add(x + 'x' + y);
 }
 
-let findclosest = (currentx, currenty) => {
-    if(wire1.has(currentx + 'x' + currenty) && (currentx != 0 && currenty != 0)) {
-        manhattan = Math.abs(currentx) + Math.abs(currenty);
+let findclosest = (x, y) => {
+    if((x != 0 && y != 0) && wirepath.has(x + 'x' + y)) {
+        manhattan = Math.abs(x) + Math.abs(y);
         if(!closest || manhattan < closest) closest = manhattan;
     }
 }
 
-let plotwire = (plot, resolve) => {
-direction = plot.charAt(0);
-    distance = parseInt(plot.substr(1));
-    switch(direction) {
-        case 'R':
-            current = currentx;
-            target = currentx + distance;
-            for(i=current; i <= target; i++) {
-                currentx = i;
-                resolve(currentx, currenty);
-            }
-            break;
-        case 'L':
-            current = currentx;
-            target = currentx - distance;
-            for(i=current; i >= target; i--) {
-                currentx = i;
-                resolve(currentx, currenty);
-            }
-            break;
-        case 'D':
-            current = currenty;
-            target = currenty + distance;
-            for(i=current; i <= target; i++) {
-                currenty = i;
-                resolve(currentx, currenty);
-            }
-            break;
-        case 'U':
-            current = currenty;
-            target = currenty - distance;
-            for(i=current; i >= target; i--) {
-                currenty = i;
-                resolve(currentx, currenty);
-            }
-            break;
-    }
-} 
-
-processwires = wire => {
-    currentx = 0,
-    currenty = 0;
+let plotwire = (wire, resolve) => {
+    x = 0,
+    y = 0;
     closest = false;
-    wire[0].split(',').forEach(plot => {
-        plotwire(plot, addwires);
+    wire.forEach(plot => {
+        direction = plot.charAt(0);
+        distance = parseInt(plot.substr(1));
+        for(i=0; i < distance; i++) {
+            switch(direction) {
+            case 'R':
+                x++
+                break;
+            case 'L':
+                x--
+                break;
+            case 'D':
+                y++
+                break;
+            case 'U':
+                y--    
+                break;
+            }
+            resolve(x, y);
+        }
     })
-    
-    currentx = 0,
-    currenty = 0;    
-    wire[1].split(',').forEach(plot => {
-        plotwire(plot, findclosest);
-    })   
-    return closest 
 }
 
+processwires = pair => {
+    plotwire(pair[0].split(','), addwires);   
+    plotwire(pair[1].split(','), findclosest);
+    return closest;
+}
 console.log(processwires(wirest1), 6)
 console.log(processwires(wirest2), 159)
 console.log(processwires(wirest3), 135)
-console.log(processwires(wires), "NOT 517")
+console.log(processwires(wires), "266?")
