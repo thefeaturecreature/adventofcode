620696c874218222ef1fc852345edbe0b70439a2
Evan Dierlam <edierlam@edierlam-nw3354.corp.nerdwallet.com>
2019-12-03 09:55:04 -0800

    day3 refac

--- 2019/day03/day03a.js
+++ 2019/day03/day03a.js
@@ -5,9 +5,7 @@ const wirest2 = ['R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D
 const wirest3 = ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51','U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'] // = 135
 
 let wire1 = new Set(),
-    currentx = 0, 
-    currenty = 0, 
-    closest = false;
+    currentx, currenty, closest;
 
 let addwires = (currentx,currenty) => {
     wire1.add(currentx + 'x' + currenty)
@@ -60,17 +58,22 @@ direction = plot.charAt(0);
 } 
 
 processwires = wire => {
+    currentx = 0,
+    currenty = 0;
+    closest = false;
     wire[0].split(',').forEach(plot => {
         plotwire(plot, addwires);
     })
     
     currentx = 0,
-    currenty = 0;
-    
+    currenty = 0;    
     wire[1].split(',').forEach(plot => {
         plotwire(plot, findclosest);
     })   
     return closest 
 }
 
-console.log(processwires(wires))
+console.log(processwires(wirest1), 6)
+console.log(processwires(wirest2), 159)
+console.log(processwires(wirest3), 135)
+console.log(processwires(wires), "NOT 517")
