df06b6c26cc8ecf8e4203358effa2d55b7f4d195
Evan <evan@debuggered.com>
2020-12-02 14:28:15 -0800

    recovery

--- 2019/day03/two.js
+++ 2019/day03/two.js
@@ -1,10 +1,9 @@
-// TODO: Incomplete (lost two. copied one. Redo!)
 const aoc = require('../../lib/aoc.js');
 const wires = inputfile('input.txt');
 
 plotwire = (pair, index) => {
-    wire = pair[index].split(',');
-    let wirepath = new Set(),
+    wire = pair[index].split(',')
+    let wirepath = [],
     x = 0,
     y = 0,
     closest = false;
@@ -14,40 +13,44 @@ plotwire = (pair, index) => {
         for(i=0; i < distance; i++) {
             switch(direction) {
             case 'R':
-                x++;
+                x++
                 break;
             case 'L':
-                x--;
+                x--
                 break;
             case 'D':
-                y++;
+                y++
                 break;
             case 'U':
-                y--;
+                y--    
                 break;
             }
-            wirepath.add(x + 'x' + y);
+            wirepath.push(x + 'x' + y);
         }
     })
-    return wirepath;
+    return wirepath
 }
 
 findpath = (match) => {
-    plot = match.split('x');
-    x = parseInt(plot[0]);
-    y = parseInt(plot[1]);
+    plot = match.split('x')
+    x = parseInt(plot[0])
+    y = parseInt(plot[1])
     return Math.abs(x) + Math.abs(y);
 }
 
 processwires = pair => {
     wirepath = plotwire(pair, 0);  
     wirepath2 = plotwire(pair, 1);
-    let closest;
-    [...wirepath]
-        .filter(a => wirepath2.has(a))
-        .forEach(match => { if(!closest || findpath(match) < closest) closest = findpath(match) });
-    return closest;
+    let steps;
+    wirepath
+        .filter(a => {return wirepath2.includes(a)})
+        .forEach(match => {
+            a = [...wirepath].indexOf(match) + 1;
+            b = [...wirepath2].indexOf(match) + 1;
+            if(!steps || (a + b) < steps) steps = (a + b);
+        })
+    return steps;
 }
 
 console.log(processwires(wires));
-// Answer: 
+// Answer: 19242
