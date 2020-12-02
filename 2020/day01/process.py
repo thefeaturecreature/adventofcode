3c0e12196edaa2f4685cd58a4298380f824c4a81
Evan <evan@debuggered.com>
2020-12-01 16:43:37 -0800

    Python Retirement Fund

--- 2020/day01/process.py
+++ 2020/day01/process.py
@@ -16,7 +16,7 @@ def day02(input):
         for second in filter(lambda x: x < diff - first  and x > first,input):
             third = diff - second
             try:
-                if third > 0 and first != second and first != third and second != third and input.index(third) >= 0:
+                if third > 0 and first != third and second != third and input.index(third) >= 0:
                     print(first * second * third)
                     break
             
