63a7cad0c207cc71ff4da20a9aa7799c81282d77
Evan <evan@debuggered.com>
2020-12-05 22:56:38 -0800

    2020 day06 and refactor

--- 2020/day06/one.js
+++ 2020/day06/one.js
@@ -3,9 +3,9 @@ const answers = inputfile('input.txt',false).split('\n\n');
 
 answergroups = answers => answers.map(group=> {
     let collector = new Set(group
-        .replace(/\n/g,'')
-        .split(''));
-    return collector.size;
+            .replace(/\n/g,'')
+            .split(''));
+        return collector.size;
     }).reduce((sum,a)=> sum+=a);
 
 console.log(answergroups(answers));
