bbe36443ffa11b93b055e906211471cf2262e945
Evan <evan@debuggered.com>
2020-12-09 21:55:53 -0800

    2020 Day 10

---
+++ 2020/day10/one.js
@@ -0,0 +1,17 @@
+const aoc = require('../../lib/aoc.js');
+const knapsack = inputfile('input.txt');
+
+let found = false,
+    connections = knapsack.length + 1,
+    topvolt = Math.max.apply(null,knapsack) + 3,
+    threes = Math.floor(topvolt / 3),
+    ones = topvolt - (threes * 3),
+    sum =  threes + ones;
+
+do {
+    threes -= 1;
+    ones += 3;
+    sum = threes + ones;
+} while (sum != connections)
+
+console.log(threes * ones)
