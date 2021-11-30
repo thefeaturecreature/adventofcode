fcb6c95efb8f1c34a46a4588e4e5988caae2389f
Evan Dierlam <evan@featurecreature.co>
2021-11-29 16:47:38 -0800

    Ready for 2021

---
+++ 2021/day01/process.py
@@ -0,0 +1,23 @@
+inputfile = sorted([int(line.replace('\n','')) for line in open('input.txt').readlines()])
+
+def day01(input):
+    for i in input:
+        try:
+            if test:
+                print("day 1: " + output)
+                break
+        except:   
+            pass
+
+def day02(input):
+    for i in input:
+        try:
+            if test:
+                print("day 1: " + output)
+                break
+        except:   
+            pass
+
+if __name__ == '__main__':
+    day01(inputfile)
+    day02(inputfile)
