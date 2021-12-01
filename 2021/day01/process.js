fa3b57ac14d4b38e788091814fc6da02227aab9b
Evan Dierlam <evan@featurecreature.co>
2021-12-01 07:19:02 -0800

    Day 1

--- 2021/day01/process.js
+++ 2021/day01/process.js
@@ -4,14 +4,14 @@ const input = inputfile('input.txt');
 
 
 part1 = input
-            .map((distance, index) => (distance - input[index-1])>0?1:0)
+            .map((distance, index, arr) => (distance - arr[index-1])>0?1:0)
             .reduce((total, diff) => total + diff)
 
 console.log("part 1:",part1);
 //1374
 
 part2 = input
-            .map((distance, index) => (input[index+2]?(parseInt(input[index+2]) + parseInt(input[index+1]) + parseInt(distance)):0))
+            .map((distance, index, arr) => (arr[index+2]?(parseInt(arr[index+2]) + parseInt(arr[index+1]) + parseInt(distance)):0))
             .map((distance, index,arr) => (distance - arr[index-1])>0?1:0)
             .reduce((total, diff) => total + diff);
 
