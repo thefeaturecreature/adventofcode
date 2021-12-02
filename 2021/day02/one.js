45f27442fbad2f38225e8a4f86b7fe7bdfeadd1e
Evan Dierlam <evan@featurecreature.co>
2021-12-01 21:34:50 -0800

    day2 + library updates

--- 2021/day02/one.js
+++ 2021/day02/one.js
@@ -1,23 +1,23 @@
 const aoc = require('../../lib/aoc.js');
 const input = inputfile('input.txt');
 
-let p1forward = 0,
-    p1depth = 0;
+let forward = 0,
+    depth = 0;
 part1 = input.forEach(e => {
     const [direction, count] = e.split(' ');
     
     switch (direction) {
         case 'forward':
-                p1forward += parseInt(count);
+                forward += parseInt(count);
             break;
         case 'up':
-                p1depth -= parseInt(count);
+                depth -= parseInt(count);
             break;
         case 'down':
-                p1depth += parseInt(count);
+                depth += parseInt(count);
             break;
     } 
 })
 
-console.log("part 1:",p1forward * p1depth);
+console.log("part 1:",forward * depth);
 //2120749
