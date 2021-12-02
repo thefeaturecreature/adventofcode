a31b6d949365aa9d482ef0475a18e845730395e8
Evan Dierlam <evan@featurecreature.co>
2021-12-01 21:31:23 -0800

    day2 + library updates

--- 2021/day01/two.js
+++ 2021/day01/two.js
@@ -1,13 +1,6 @@
 const aoc = require('../../lib/aoc.js');
 const input = inputfile('input.txt');
 
-part1 = input
-            .map((distance, index, arr) => (distance - arr[index-1])>0?1:0)
-            .reduce((total, diff) => total + diff)
-
-console.log("part 1:",part1);
-//1374
-
 part2 = input
             .map((distance, index, arr) => (arr[index+2]?(parseInt(arr[index+2]) + parseInt(arr[index+1]) + parseInt(distance)):0))
             .map((distance, index,arr) => (distance - arr[index-1])>0?1:0)
