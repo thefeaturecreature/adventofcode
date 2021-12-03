307c54dc37a4987840f0c478069a3868cbc71a8e
Evan Dierlam <evan@featurecreature.co>
2021-12-03 07:42:31 -0800

    day 2 refactor

--- 2021/day03/one.js
+++ 2021/day03/one.js
@@ -5,7 +5,10 @@ const input = inputfile('input.txt');
 let gamma = "",
     epsilon = "";
     
-totalat = position => input.map(e => parseInt(e.charAt(position))).reduce((e,f) => e+f);
+totalat = position => input
+                            .map(e => parseInt(e.charAt(position)))
+                            .reduce((e,f) => e+f);
+                            
 greaterat = position => totalat(position)>(input.length/2)?1:0;
 lesserat = position => totalat(position)>(input.length/2)?0:1;
 
@@ -14,5 +17,5 @@ for (var i = 0; i < input[0].length; i++) {
     epsilon += lesserat(i)
 }
 
-console.log(parseInt(gamma, 2) * parseInt(epsilon, 2) == 741950)
+console.log(parseInt(gamma, 2) * parseInt(epsilon, 2))
 //741950
--- 2021/day03/two.js
+++ 2021/day03/two.js
@@ -5,16 +5,28 @@ const input = inputfile('input.txt');
 let oxygen = input,
     bit = 0;
 do {
-    totalat = position => oxygen.map(e => parseInt(e.substr(position,1))).reduce((e,f) => e+f);
-    oxygen = oxygen.filter(e => e.substr(bit,1) == totalat(bit)>=(oxygen.length/2)?1:0);
+    totalat = position => oxygen
+                                .map(e => parseInt(e.charAt(position)))
+                                .reduce((e,f) => e+f);
+
+    oxygen = oxygen.filter(e => {
+        greaterat = position => totalat(position)>=(oxygen.length/2)?1:0;
+        return e.charAt(bit) == greaterat(bit);
+    })
     bit++
 } while(oxygen.length > 1)
 
 let co2 = input,
     cobit = 0;
 do {
-    totalat = position => co2.map(e => parseInt(e.substr(position,1))).reduce((e,f) => e+f);
-    co2 = co2.filter(e => e.substr(cobit,1) == totalat(cobit)>=(co2.length/2)?0:1);
+    totalat = position => co2
+                            .map(e => parseInt(e.charAt(position)))
+                            .reduce((e,f) => e+f);
+                            
+    co2 = co2.filter(e => {
+        lesserat = position => totalat(position)>=(co2.length/2)?0:1;        
+        return e.charAt(cobit) == lesserat(cobit);
+    });
         cobit++
 } while(co2.length > 1)
 
