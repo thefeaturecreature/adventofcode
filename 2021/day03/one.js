94c68eb82aa17fc0038825d4341bb59fb968f0b2
Evan Dierlam <evan@featurecreature.co>
2021-12-03 07:28:29 -0800

    day 1 refactor

--- 2021/day03/one.js
+++ 2021/day03/one.js
@@ -5,12 +5,14 @@ const input = inputfile('input.txt');
 let gamma = "",
     epsilon = "";
     
-totalat = position => input.map(e => parseInt(e.substr(position,1))).reduce((e,f) => e+f);
+totalat = position => input.map(e => parseInt(e.charAt(position))).reduce((e,f) => e+f);
+greaterat = position => totalat(position)>(input.length/2)?1:0;
+lesserat = position => totalat(position)>(input.length/2)?0:1;
 
 for (var i = 0; i < input[0].length; i++) {
-    gamma += (totalat(i)>(input.length/2)?1:0);
-    epsilon += (totalat(i)>(input.length/2)?0:1)
+    gamma += greaterat(i);
+    epsilon += lesserat(i)
 }
 
-console.log(parseInt(gamma, 2) * parseInt(epsilon, 2))
+console.log(parseInt(gamma, 2) * parseInt(epsilon, 2) == 741950)
 //741950
