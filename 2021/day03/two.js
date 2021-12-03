4910fc947382a606262b840758fd1b7c1b3cb7d3
Evan Dierlam <evan@featurecreature.co>
2021-12-03 08:20:06 -0800

    day 2 refactor

--- 2021/day03/two.js
+++ 2021/day03/two.js
@@ -1,23 +1,25 @@
 const aoc = require('../../lib/aoc.js');
 const input = inputfile('input.txt');
 
-totalat = (array, position) => array.map(e => parseInt(e.charAt(position)))
+const totalat = (array, position) => array.map(e => parseInt(e.charAt(position)))
                                     .reduce((e,f) => e+f);
 
-greaterat = (array, position) => totalat(array, position)>=(array.length/2)?1:0;
-lesserat = (array, position) => totalat(array, position)>=(array.length/2)?0:1;        
+const greaterat = (array, position) => totalat(array, position)>=(array.length/2)?1:0;
+const lesserat = (array, position) => totalat(array, position)>=(array.length/2)?0:1;        
 
 let oxygen = input,
     bit = 0;
 do {
-    oxygen = oxygen.filter(e => e.charAt(bit) == greaterat(oxygen, bit));
+    const greater = greaterat(oxygen, bit);
+    oxygen = oxygen.filter(e => e.charAt(bit) == greater);
     bit++;
 } while(oxygen.length > 1)
 
 let co2 = input,
     cobit = 0;
 do {
-    co2 = co2.filter(e => e.charAt(cobit) == lesserat(co2, cobit));
+    const lesser = lesserat(co2, cobit);
+    co2 = co2.filter(e => e.charAt(cobit) == lesser);
     cobit++;
 } while(co2.length > 1)
 
