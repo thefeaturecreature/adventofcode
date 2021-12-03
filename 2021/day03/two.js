fc8d4e0e8d29ec95cf3940a46903c0733f8ec479
Evan Dierlam <evan@featurecreature.co>
2021-12-03 08:08:02 -0800

    day 2 refactor

--- 2021/day03/two.js
+++ 2021/day03/two.js
@@ -3,27 +3,22 @@ const input = inputfile('input.txt');
 
 let oxygen = input,
     bit = 0;
+
+totalat = (arr, position) => arr.map(e => parseInt(e.charAt(position)))
+                            .reduce((e,f) => e+f);
+
+greaterat = (arr, position) => totalat(arr, position)>=(arr.length/2)?1:0;
+lesserat = (arr, position) => totalat(arr, position)>=(co2.length/2)?0:1;        
+
 do {
-    totalat = position => oxygen.map(e => parseInt(e.charAt(position)))
-                                .reduce((e,f) => e+f);
-
-    greaterat = position => totalat(position)>=(oxygen.length/2)?1:0;
-
-    oxygen = oxygen.filter(e => e.charAt(bit) == greaterat(bit))
-    
+    oxygen = oxygen.filter(e => e.charAt(bit) == greaterat(oxygen, bit))    
     bit++
 } while(oxygen.length > 1)
 
 let co2 = input,
     cobit = 0;
 do {
-    totalat = position => co2.map(e => parseInt(e.charAt(position)))
-                             .reduce((e,f) => e+f);
-
-    lesserat = position => totalat(position)>=(co2.length/2)?0:1;        
-
-    co2 = co2.filter(e => e.charAt(cobit) == lesserat(cobit));
-    
+    co2 = co2.filter(e => e.charAt(cobit) == lesserat(co2, cobit));
     cobit++
 } while(co2.length > 1)
 
