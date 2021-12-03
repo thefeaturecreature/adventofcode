f3feed76b2b3fc9c57c01fd0f64fc435c91979b5
Evan Dierlam <evan@featurecreature.co>
2021-12-03 07:58:09 -0800

    day 2 refactor

--- 2021/day03/two.js
+++ 2021/day03/two.js
@@ -7,10 +7,10 @@ do {
     totalat = position => oxygen.map(e => parseInt(e.charAt(position)))
                                 .reduce((e,f) => e+f);
 
-    oxygen = oxygen.filter(e => {
-        greaterat = position => totalat(position)>=(oxygen.length/2)?1:0;
-        return e.charAt(bit) == greaterat(bit);
-    })
+    greaterat = position => totalat(position)>=(oxygen.length/2)?1:0;
+
+    oxygen = oxygen.filter(e => e.charAt(bit) == greaterat(bit))
+    
     bit++
 } while(oxygen.length > 1)
 
@@ -20,11 +20,11 @@ do {
     totalat = position => co2.map(e => parseInt(e.charAt(position)))
                              .reduce((e,f) => e+f);
 
-    co2 = co2.filter(e => {
-        lesserat = position => totalat(position)>=(co2.length/2)?0:1;        
-        return e.charAt(cobit) == lesserat(cobit);
-    });
-        cobit++
+    lesserat = position => totalat(position)>=(co2.length/2)?0:1;        
+
+    co2 = co2.filter(e => e.charAt(cobit) == lesserat(cobit));
+    
+    cobit++
 } while(co2.length > 1)
 
 console.log(parseInt(oxygen,2) * parseInt(co2,2));
